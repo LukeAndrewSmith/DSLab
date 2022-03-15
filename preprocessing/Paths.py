e089c25c0918f5a2e5d4359ed7dfadcd42257d58
lukeasmi <lukeasmi@cisco.com>
2022-03-15 17:33:27 +0100

    TODO: paths

---
+++ preprocessing/Paths.py
@@ -0,0 +1,10 @@
+# TODO: Add all paths to here so that we don't hardcode them anyher
+
+class Paths:
+    def __init__(self):
+        self.data = ''
+        self.generated_datasets = ''
+        self.images = ''
+        self.labelme_jsons = ''
+        self.point_labels = ''
+        self.width_measurements = ''
