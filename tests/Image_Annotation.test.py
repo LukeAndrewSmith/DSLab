f28397ba8770db47d7e20d0b95c6f33b37b206ac
boeselfr <boesel.frederic@googlemail.com>
2022-03-15 10:46:47 +0100

    added draw function to check fiunctinality

--- tests/Image_Annotation.test.py
+++ tests/Image_Annotation.test.py
@@ -1,5 +1,30 @@
 from preprocessing.Image_Annotation import Image_Annotation
 import cv2
+import os
+import numpy as np
+
+def draw_all_image_annotations(annotation: Image_Annotation):
+    image = cv2.imread(os.path.join("/Users/fredericboesel/Documents/master/frühling22/ds_lab/data/labels",annotation.image_path))
+
+    for core in annotation.core_annotations:
+        cv2.drawContours(image, [np.int0(np.array(core.inner_bound))], -1, (0,0,0), 5)
+        cv2.drawContours(image, [np.int0(np.array(core.outer_bound))], -1, (0,0,0), 5)
+        cv2.drawContours(image, [np.int0(np.array(core.bark))], -1 ,(0,0,0), 5)
+        cv2.drawContours(image, [np.int0(np.array(core.ctrmid))], -1, (0,0,0), 5)
+        cv2.drawContours(image, [np.int0(np.array(core.ctrend))], -1, (0,0,0), 5)
+        # cracks:
+        for crack in core.cracks:
+            cv2.drawContours(image, [np.int0(np.array(crack))], -1, (0,0,0), 5)
+        for ring in core.rings:
+            # can be multiple points on one ring
+            for point in ring:
+                cv2.circle(image, np.int0(np.array(point)), radius=5, color=(0,0,0), thickness=10)
+
+    cv2.imshow('test', image)
+    cv2.imwrite('goodass.jpg', image)
+    cv2.waitKey(0)
+
+
 
 if __name__ == "__main__":
     pos_path = "/Users/fredericboesel/Documents/master/frühling22/ds_lab/data/Data label pos files"
@@ -10,3 +35,5 @@ if __name__ == "__main__":
 
     # draw rectangles and points
     print(annotation)
+    draw_all_image_annotations(annotation)
+
