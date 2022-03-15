8f0755f2f47ec97d315aefb5482fc608ebb90584
lukeasmi <lukeasmi@cisco.com>
2022-03-15 17:32:47 +0100

    Dataset Extractor progress

--- preprocessing/Dataset_Extractor.py
+++ preprocessing/Dataset_Extractor.py
@@ -1,19 +1,21 @@
 import os
-from Image_Annotation import Image_Annotation
-from Core_Annotation import Core_Annotation
-from preprocessing.geometry import mm_to_pixel, pixel_to_mm
+import numpy as np
+import cv2
+from preprocessing.Image_Annotation import Image_Annotation
+from preprocessing.geometry import mm_to_pixel
 
 class Dataset_Extractor:
 # Class to extract datasets from image and core annotations
 
     def __init__(self):
-        self._initCoreAnnotations()
+        self.coreAnnotations = self._initCoreAnnotations()
 
     def _initCoreAnnotations(self):
-        self.coreAnnotations = []
+        coreAnnotations = []
         dir = 'data/labelme_jsons/'
         for file in os.listdir(dir):
-            self.coreAnnotations = self.coreAnnotations + Image_Annotation(dir+file, 'data/point_labels').core_annotations
+            coreAnnotations = coreAnnotations + Image_Annotation(dir+file, 'data/point_labels').core_annotations
+        return coreAnnotations
 
     def createInnerDataset(self):
         self._apply(self._processCore, self.coreAnnotations)
@@ -24,65 +26,58 @@ class Dataset_Extractor:
         # Rotate rectangle/points
         # Crop image
         # Save image and pos file
-        self._shiftPoints(core)
-        self._rotateImageAndPoints()
-        self._cropImage()
-        self._saveImage()
 
-    def _shiftPoints(self):
-        pass
+        core = self._convertMMToPX(core)
+        core = self._rotateImageAndPoints(core)
+        core = self._shiftPoints(core)
+        core = self._cropImage(core)
+        core = self._saveImage(core)
 
-    def _rotateImageAndPoints(self):
-        pass
+    def _convertMMToPX(self, core):
+        # TODO: Pixels are indexed from where?
+        core.pointLabels = [[[mm_to_pixel(coord, core.dpi) for coord in coords]
+                                                           for coords in shape] 
+                                                           for shape in core.pointLabels]
+        core.gapLabels = [[[mm_to_pixel(coord, core.dpi) for coord in coords]
+                                                         for coords in shape] 
+                                                         for shape in core.gapLabels]
+        core.distToPith = mm_to_pixel(core.distToPith, core.dpi)
 
-    def _cropImage(self):
-        pass
+    def _rotateImageAndPoints(self, core):
+        # print(core.innerRectangle)
+        # NOTE: Points are stored in order: counter-clockwise from the top left coordinate:
+        # 0 ----- 3
+        # |       |
+        # 1 ----- 2
+        [topLeft, bottomLeft, topRight, bottomRight] = core.innerRectangle
+        run  = topRight[0] - topLeft[0]
+        rise = topRight[1] - topLeft[1] # TODO: (-ve or +ve)? if 3 heigher than 0, negative otherwise
+        print("rise:", rise)
+        print("run:", run)
+        angle = np.degrees(np.arctan(rise/run))
+        print('angle:', angle)
+        center = (int(topLeft[0] + run/2), int(topLeft[1] + rise/2)) # TODO: Assuming top left pixel is (0,0)
+        print('center:', center) # TODO: verify this center point
+        cv2.getRotationMatrix2D(center, angle, 1.0)
+        return core
 
-    def _saveImage(self):
-        pass
+    def rotatePoints(image, angle):
+        image_center = tuple(np.array(image.shape[1::-1]) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+        result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+        return result
+
+    def _shiftPoints(self, core):
+        return core
+
+    def _cropImage(self, core):
+        return core
+
+    def _saveImage(self, core):
+        return core
 
     ###############
     # Helpers
     def _apply(self, func, list):
         for item in list:
             func(item)
-
-# USEFUL CODE FOR CONVERTING MM TO PX
-# def get_pos_info(self):
-    #     rings = list()
-    #     dpi = 0
-    #     pith = None
-    #     dist_to_pith = None
-    #     years_to_pith = None
-    #     for file in os.listdir(self.pos_path):
-    #         if file == f'{self.name}.pos':
-    #             f = open(os.path.join(self.pos_path, file))
-    #             for line in f.readlines():
-    #                 s = list(filter(None,re.split("[ \n;]", line)))
-    #                 if s[0] == '#DPI':
-    #                     dpi = float(s[1])
-    #                 if len(s) > 1:
-    #                     if 'Pith' in s[1]:
-    #                         # looks like this:
-    #                         # #C PithCoordinates=447.146,70.294; DistanceToPith=50.8; YearsToPith=13;
-    #                         pith_mm = s[1].split('=')[1].split(',')
-    #                         # to pixel values:
-    #                         pith = [mm_to_pixel(float(coordinate), dpi) for coordinate in pith_mm]
-
-    #                         dist_to_pith_mm = s[2].split('=')[1]
-    #                         # to pixel values:
-    #                         dist_to_pith = mm_to_pixel(float(dist_to_pith_mm), dpi)
-
-    #                         years_to_pith = int(float(s[3].split('=')[1]))
-    #                 if '#' not in s[0] and 'SCALE' not in s[0]:
-    #                     ring_coordinates = s
-    #                     # there can be multiple points on one ring
-    #                     ring = list()
-    #                     for point in ring_coordinates:
-    #                         point = point.split(',')
-    #                         ring_point = [mm_to_pixel(float(coordinate), dpi) for coordinate in point]
-    #                         ring.append(ring_point)
-    #                     rings.append(ring)
-
-    #     return rings, dpi, pith, dist_to_pith, years_to_pith
-
