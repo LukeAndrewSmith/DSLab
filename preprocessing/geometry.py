8e4be13f1442c75248d84f56004743d1be4774c1
lukeasmi <lukeasmi@cisco.com>
2022-03-15 17:33:14 +0100

    Fix min bounding rectangle

--- preprocessing/geometry.py
+++ preprocessing/geometry.py
@@ -5,7 +5,7 @@ import json
 
 def min_bounding_rectangle(points):
     # converts a list of points into the min are rectangle containing all points
-    points = np.asarray(points)
+    points = np.int0(np.asarray(points)) # TODO: understand why np.int0 works
     rect = cv2.minAreaRect(points)
     box = cv2.boxPoints(rect)
     box = np.int0(box)
