c2d153fdc17efa7a55ea7b50a0c13591d138511c
Clement Guerner <cguerner@Clements-MacBook-Pro.local>
2022-03-24 19:21:41 +0100

    fixed pathing

--- src/ringdetector/ringdetector/Paths.py
+++ src/ringdetector/ringdetector/Paths.py
@@ -1,6 +1,6 @@
 import os
 
-DATA = '/Users/cguerner/Documents/classes/dslab/dslabtreering/data'
+DATA = '../../../data'
 GENERATED_DATASETS = os.path.join(DATA, "generated_datasets")
 GENERATED_DATASETS_INNER = os.path.join(DATA, 'generated_datasets/inner/')
 IMAGES = os.path.join(DATA, 'annotated_images/')
