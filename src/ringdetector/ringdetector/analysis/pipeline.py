94ecb2df08c14651f2190cb67f8e492e38764080
Clement Guerner <cguerner@Clements-MacBook-Pro.local>
2022-04-05 19:06:56 +0200

    updated pipeline

--- src/ringdetector/ringdetector/analysis/pipeline.py
+++ src/ringdetector/ringdetector/analysis/pipeline.py
@@ -3,16 +3,26 @@ from ringdetector.Paths import GENERATED_DATASETS_INNER, DATA
 import os
 from ringdetector.analysis.EdgeProcessor import EdgeProcessor
 
+from ringdetector.analysis.CoreProcessor import CoreProcessor
+from ringdetector.Paths import GENERATED_DATASETS_INNER
+
+TEST_SAMPLE = "KunA01SS"
 
 if __name__ == "__main__":
-    impath = os.path.join(GENERATED_DATASETS_INNER, "KunA08NN.jpg")
-    Im = ImageProcessor(impath)
-    Im.denoiseImage()
-    Im.computeGradients(method='Canny', threshold1=50, threshold2=100)
+    cp = CoreProcessor(TEST_SAMPLE)
+
+    cp.scoreCore()
+    cp.exportCoreImg(os.path.join(GENERATED_DATASETS_INNER, "results"))
+
+    # Freddy's old pipeline
+    #impath = os.path.join(GENERATED_DATASETS_INNER, "KunA08NN.jpg")
+    #Im = ImageProcessor(impath)
+    #Im.denoiseImage()
+    #Im.computeGradients(method='Canny', threshold1=50, threshold2=100)
     #Im.normalizeGradients()
-    Im.plotGradientTimeSeries(Im.gXY)
+    #Im.plotGradientTimeSeries(Im.gXY)
     #Im.saveImage(Im.gXY, os.path.join(DATA, f"{Im.name}_grad_canny.jpg"))
-    edgeProcessor = EdgeProcessor(Im.gXY)
-    edgeProcessor.processEdgeInstances(minLength=50)
-    instancepath = os.path.join(DATA, f"{Im.name}_grad_canny_instances_filtered_50.jpg")
-    edgeProcessor.saveEdgeInstanceImage(instancepath)
+    #edgeProcessor = EdgeProcessor(Im.gXY)
+    #edgeProcessor.processEdgeInstances(minLength=50)
+    #instancepath = os.path.join(DATA, f"{Im.name}_grad_canny_instances_filtered_50.jpg")
+    #edgeProcessor.saveEdgeInstanceImage(instancepath)
