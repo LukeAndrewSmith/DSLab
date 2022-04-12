db82acdc53fc05452e679abdb4cfa7275c01516f
lukeasmi <lukeasmi@cisco.com>
2022-04-12 20:27:32 +0200

    Rename

--- src/ringdetector/ringdetector/analysis/EdgeProcessor.py
+++ src/ringdetector/ringdetector/analysis/EdgeProcessor.py
@@ -8,46 +8,46 @@ from ringdetector.analysis.Edge import Edge
 
 class EdgeProcessor():
     '''
-        Gets an candidate edges from one of the Edge detection algos and postprocesses 
-        to find edges
+        Gets an candidateEdges (grascale image) from one of the Edge detection algos 
+        and postprocesses to identify edges
     '''
     def __init__(self, candidateEdges, cfg):
         self.edges = self.__getEdges(candidateEdges, cfg)
 
         
     def __getEdges(self, candidateEdges, cfg):
-        binaryEdgeMatrix = self.__getBinaryEdgeMatrix(candidateEdges)
+        candidateEdgesMask = self.__getCandidateEdgesMask(candidateEdges)
         
         s = [[1,1,1],                                       # neighborhood definition
             [1,1,1],
             [1,1,1]]
-        labels, numL = label(binaryEdgeMatrix, structure=s) # Label the contiguous shapes
+        labels, numL = label(candidateEdgesMask, structure=s) # Label the contiguous shapes
         shapes = [[] for _ in range(numL+1)]                # Create an array per shape
         [[shapes[labels[i,j]].append((i,j)) for i in range(candidateEdges.shape[0])] 
                                             for j in range(candidateEdges.shape[1])] # Append index to the shapes array
         shapes = shapes[1:]                            # 0 label => no shape, didn't but an 'if' in the previous line as it's much slower
 
-        return self.processEdgeInstances(binaryEdgeMatrix, shapes, cfg)
+        return self.identifyEdges(candidateEdgesMask, shapes, cfg)
     
 
-
-    def __getBinaryEdgeMatrix(self, edges):
+    def __getCandidateEdgesMask(self, edges):
         # converts to binary for further processing:
         # assumes edges to be min=0, max=? prob 255
-        binaryEdgeMatrix = np.around(
+        candidateEdgesMask = np.around(
             edges/np.max(edges), decimals=0).astype(int)
-        return binaryEdgeMatrix
+        return candidateEdgesMask
 
-    ### Process shapes into Edges
-    def processEdgeInstances(self, binaryEdgeMatrix, shapes, cfg):
+
+    def identifyEdges(self, candidateEdgesMask, shapes, cfg):
         #TODO: try linking edge instances like in the paper
         filteredShapes = self.__filterByLength(shapes, cfg.minedgelen)
         edges = []
         for shape in filteredShapes:
-            edge = Edge(shape, binaryEdgeMatrix.shape)
+            edge = Edge(shape, candidateEdgesMask.shape)
             edge.fitPredict(model=cfg.edgemodel)
             edges.append(edge)
         return edges
+
 
     def __filterByLength(self, shapes, minLength):
         filteredShapes = [
@@ -56,10 +56,13 @@ class EdgeProcessor():
         ]
         return filteredShapes
 
+
     def scoreEdges(self, pointLabels):
         for edge in self.edges:
             edge.scoreEdge(pointLabels)
 
+
+    ###########################################################
     ### Plotting functions
     def ImageFromEdgeInstances(self, edgeType="all"):
         """ Create black and white image displaying edge instances. 
