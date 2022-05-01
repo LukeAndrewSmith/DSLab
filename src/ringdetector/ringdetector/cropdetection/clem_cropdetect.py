f57900af618f9596dd046b7a280b5825f1550715
Clement Guerner <cguerner@student-net-vpn-2681.intern.ethz.ch>
2022-05-01 14:09:07 +0200

    updated

--- src/ringdetector/ringdetector/cropdetection/clem_cropdetect.py
+++ src/ringdetector/ringdetector/cropdetection/clem_cropdetect.py
@@ -8,7 +8,9 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog, DatasetCatalog
 
 from ringdetector.cropdetection.D2CustomDataset import D2CustomDataset
+from ringdetector.cropdetection.utils import get_cuda_info
 from ringdetector.Paths import LABELME_JSONS, POINT_LABELS, D2_RESULTS
+
 
 setup_logger()
 
@@ -17,8 +19,8 @@ setup_logger()
 ds = D2CustomDataset(
     json_path=LABELME_JSONS, pos_path=POINT_LABELS
 )
-dataset = ds.generate_dataset("train", "inner", False)
-name = "train"
+#dataset = ds.generate_dataset("train", "inner", False)
+name = "innercrop_train"
 DatasetCatalog.register(name, lambda d="train":ds.generate_dataset(d, "inner", False))
 metadata = MetadataCatalog.get(name)
 metadata.set(thing_classes=[f"inner_false"])
@@ -44,7 +46,7 @@ cfg = get_cfg()
 cfg.merge_from_file(
     model_zoo.get_config_file(
         "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("inner_false",)
+cfg.DATASETS.TRAIN = (name,)
 cfg.DATASETS.TEST = ()
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
@@ -55,6 +57,9 @@ cfg.SOLVER.STEPS = []        # do not decay learning rate
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
 # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+#%%
+print(get_cuda_info())
 
 #%%
 cfg.OUTPUT_DIR = D2_RESULTS
